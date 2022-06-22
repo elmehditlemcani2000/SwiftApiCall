e5ca0851344065dd65faf74c6d09797b56c595af
elmehditlemcani2000 <elmehditlemcani@gmail.com>
2022-06-22 10:18:40 +0200

    Initial Commit

---
+++ SwiftApiCall/SwiftApiCallApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftApiCallApp.swift
+//  SwiftApiCall
+//
+//  Created by user on 22/06/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftApiCallApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
